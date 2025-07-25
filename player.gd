4d2d5cbcc8c04afdac295c2c4b5e1008ba2fee92
Menelik Wubneh <mwubneh@z3r6p3.42lyon.fr>
2025-07-25 04:15:16 +0200

    fixed deplacement issues

--- player.gd
+++ player.gd
@@ -1,13 +1,13 @@
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
 @export var speed = 14
-# The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
 
 var target_velocity = Vector3.ZERO
+
 @onready var camera_pivot = $CameraPivot
 @onready var player_pivot = $"."
+
 var mouse_sensitivity = 0.005
 var rotation_x = 0.0
 var rotation_y = 0.0
@@ -19,33 +19,36 @@ func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotation_y -= event.relative.x * mouse_sensitivity
 		rotation_x = clamp(rotation_x - event.relative.y * mouse_sensitivity, deg_to_rad(-45), deg_to_rad(45))
-		#camera_pivot.rotation = Vector3(rotation_x, 0, 0)
+		camera_pivot.rotation = Vector3(rotation_x, 0, 0)
 		player_pivot.rotation = Vector3(0, rotation_y, 0)
+
 func _physics_process(delta):
-	var direction = Vector3.ZERO
+	var input_dir = Vector3.ZERO
 
 	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
+		input_dir.x -= 1
 	if Input.is_action_pressed("move_right"):
-		direction.x += 1
+		input_dir.x += 1
 	if Input.is_action_pressed("move_back"):
-		direction.z += 1
+		input_dir.z += 1
 	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+		input_dir.z -= 1
 	if Input.is_action_pressed("quit"):
 		get_tree().quit()
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-#		$Pivot.basis = Basis.looking_at(direction)
 
-	# Ground Velocity
+	# Transformation de la direction selon la rotation du joueur
+	var direction = (player_pivot.transform.basis * input_dir).normalized()
+
+	# Vitesse horizontale
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
 
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	# Gravité
+	if not is_on_floor():
+		target_velocity.y -= fall_acceleration * delta
+	else:
+		target_velocity.y = 0.0  # reset si au sol
 
-	# Moving the Character
+	# Déplacement
 	velocity = target_velocity
 	move_and_slide()
